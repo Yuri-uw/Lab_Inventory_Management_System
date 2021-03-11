f9d5a6c3eb715b9e66d2b2659b8044924073aff6
Yuri-uw <73818437+Yuri-uw@users.noreply.github.com>
2021-03-10 23:08:02 -0800

    Update AlertInfo.java

--- src/util/AlertInfo.java
+++ src/util/AlertInfo.java
@@ -1,12 +1,13 @@
-package util;
-
-import javafx.scene.control.Alert;
-
 /**
  * @Description: The AlertInfo is used to show a alert dialog
  * @Author: Li Liao
  * @Date: 03/07/2021
  */
+
+package util;
+
+import javafx.scene.control.Alert;
+
 public class AlertInfo {
 
     /**
