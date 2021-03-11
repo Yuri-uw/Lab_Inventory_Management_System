41eeba06c874d81f63bef7bb2e9d17f71cbb1fb7
Yuri-uw <73818437+Yuri-uw@users.noreply.github.com>
2021-03-10 22:45:01 -0800

    Update DatabaseHandler.java

--- src/database/DatabaseHandler.java
+++ src/database/DatabaseHandler.java
@@ -1,17 +1,16 @@
-package database;
-
-import javax.swing.*;
-import java.sql.*;
-import java.util.HashSet;
-import java.util.Set;
-
 /**
- * @Description: Database connection
  * @Author: Li Liao
  * @Date: 03/04/2021
  */
-public final class DatabaseHandler {
 
+package database;
+
+import java.sql.*;
+
+/**
+ * DatabaseHandler is used to handle request 
+ */
+public final class DatabaseHandler {
 
     private static DatabaseHandler handler = null;
     static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
@@ -20,47 +19,85 @@ public final class DatabaseHandler {
     static final String PASS = "your_password";
     private static Connection connection = null;
     private static Statement statement = null;
-
+    
+    /**
+     * A static method when connect to the database when initializating
+     */
     static {
         createConnection();
     }
 
     private DatabaseHandler() {
     }
-
+    
+    /**
+     * This method used to connect with database 
+     */
+    private static void createConnection() {
+        try {
+            Class.forName(JDBC_DRIVER);
+            connection = DriverManager.getConnection(DB_URL, USER, PASS);
+            statement = connection.createStatement();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            System.exit(0);
+        }
+    }
+    
+    /**
+     * This method used to get DatabaseHandler
+     */
     public static DatabaseHandler getInstance() {
         if (handler == null) {
             handler = new DatabaseHandler();
         }
         return handler;
     }
-
-    private static void createConnection() {
-        try {
-            Class.forName(JDBC_DRIVER);
-            connection = DriverManager.getConnection(DB_URL, USER, PASS);
-            statement = connection.createStatement();
-            //System.out.println("database connect success");
-        } catch (Exception exception) {
-            exception.printStackTrace();
-            System.exit(0);
-        }
+    
+    /**
+     * This method used to close connetion when quit program
+     */
+    public Connection getConnection() {
+        return connection;
     }
-
+    
+     /**
+     * This method is called to handle SQL statement, and return ResultSet
+     * @param query A SQL statement trigger by action on GUI
+     * @return A ResultSet object that contains the data
+     */
     public ResultSet execQuery(String query) {
         ResultSet result;
-        // System.out.println("execQuery:dataHandler" + query);
         try {
             statement = connection.createStatement();
-            result = statement.executeQuery(query);   // return single
+            result = statement.executeQuery(query); 
         } catch (SQLException exception) {
             exception.printStackTrace();
-            // System.out.println("Exception at execQuery:dataHandler" + exception.getLocalizedMessage());
             return null;
         }
         return result;
     }
-
+    
+    /**
+     * This method is called to handle SQL statement, and return a int to trigger Alert dialog
+     * @param query A SQL statement trigger by action on GUI
+     * @return The row count for SQL DML statements
+     */
+    public int execUpdate(String query) {
+        try {
+            statement = connection.createStatement();
+            return statement.executeUpdate(query);
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+            return 0;
+        }
+    }
+    
+    /**
+     * This method is called to handle SQL statement
+     * @param query A SQL statement trigger by action on GUI
+     * @return A boolean
+     */
     public boolean execAction(String query) {
         try {
             statement = connection.createStatement();
@@ -70,50 +107,5 @@ public final class DatabaseHandler {
             exception.printStackTrace();
             return false;
         }
-    }
-
-    public int execUpdate(String query) {
-        // System.out.println("execQuery:dataHandler:" + query);
-        try {
-            statement = connection.createStatement();
-            return statement.executeUpdate(query);
-        } catch (SQLException exception) {
-            exception.printStackTrace();
-            return 0;
-        }
-    }
-
-
-    /*
-    private static Set<String> getDBTables() throws SQLException {
-        Set<String> set = new HashSet<>();
-        DatabaseMetaData metaData = connection.getMetaData();
-        readDBTable(set, metaData, "TABLE", null);
-        return set;
-    }
-
-    private static void readDBTable(Set<String> set, DatabaseMetaData metaData, String searchCriteria, String schema) throws SQLException {
-        ResultSet rs = metaData.getTables(null, schema, null, new String[]{searchCriteria});
-        while (rs.next()) {
-            set.add(rs.getString("TABLE_NAME").toLowerCase());
-        }
-    }
-
-    public boolean execAction(String qu) {
-        try {
-            statement = connection.createStatement();
-            statement.execute(qu);
-            return true;
-        } catch (SQLException exception) {
-            JOptionPane.showMessageDialog(null, "Error:" + exception.getMessage(), "Error Occured", JOptionPane.ERROR_MESSAGE);
-            System.out.println("Exception at execQuery:dataHandler" + exception.getLocalizedMessage());
-            return false;
-        } finally {
-        }
-    }
-     */
-
-    public Connection getConnection() {
-        return connection;
     }
 }
